81647178e5f0b47470bb075c5af146fee0d1b501
Carla Rani <carla.rani@gmail.com>
2022-06-17 12:31:12 -0300

    Exercicio 5

--- Funcionario.cs
+++ Funcionario.cs
@@ -28,6 +28,13 @@ namespace ExHeranca_POO
             Salario = Salario * 1.1;
             return Salario;
         }
+
+        public static void imprimirDados(Funcionario funcionario)
+        {
+            Console.WriteLine($"O salário de {funcionario.Nome} é R${Math.Round(funcionario.Salario, 2)}");
+            Console.WriteLine($"Após bonificação, o salário de {funcionario.Nome} passou para R${Math.Round(Funcionario.calcularBonificacao(funcionario.Salario), 2)}");
+            Console.WriteLine(Environment.NewLine);        
+        }
     }
     
     
@@ -40,24 +47,19 @@ namespace ExHeranca_POO
             Funcionario func03 = new Funcionario("Denise", 7000);
             Funcionario func04 = new Funcionario("Diana", 4500);
             Funcionario func05 = new Funcionario("Duke", 3800);
-
-
-            Console.WriteLine($"O salário de {func01.Nome} é R${func01.Salario}");
-            Console.WriteLine($"O salário de {func02.Nome} é R${func02.Salario}");
-            Console.WriteLine($"O salário de {func03.Nome} é R${func03.Salario}");
-            Console.WriteLine($"O salário de {func04.Nome} é R${func04.Salario}");
-            Console.WriteLine($"O salário de {func05.Nome} é R${func05.Salario}");
-
             Funcionario func06 = new Gerente("Fernanda", 10000);
             Funcionario func07 = new Telefonista("Fabiano", 2000);
             Funcionario func08 = new Secretaria("Fuller", 4500);
 
-            Console.WriteLine($"GERENTE: O salário de {func06.Nome} é R${func06.Salario}");
-            Console.WriteLine($"TELEFONISTA: O salário de {func07.Nome} é R${func07.Salario}");
-            Console.WriteLine($"SECRETARIA: O salário de {func08.Nome} é R${func08.Salario}");
+            Funcionario.imprimirDados(func01);
+            Funcionario.imprimirDados(func02);
+            Funcionario.imprimirDados(func03);
+            Funcionario.imprimirDados(func04);
+            Funcionario.imprimirDados(func05);
+            Funcionario.imprimirDados(func06);
+            Funcionario.imprimirDados(func07);
+            Funcionario.imprimirDados(func08);
 
-            Console.WriteLine($"O salário de {func01.Nome} é R${func01.Salario}");
-            Console.WriteLine($"Após bonificação, o salário de {func01.Nome} passou para R${Funcionario.calcularBonificacao(func01.Salario)}");
 
         }
     }
